3730ae21289c791e01161251be7abdfefeab167d
lifework <kobayashi@lifework.tech>
2022-09-20 10:16:26 +0900

    Prism で作り直し

--- WPF_Mutex_NamedPipe/App.xaml.cs
+++ WPF_Mutex_NamedPipe/App.xaml.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
 using System.Windows;
+using WPF_Mutex_NamedPipe.Views;
 
 namespace WPF_Mutex_NamedPipe
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
