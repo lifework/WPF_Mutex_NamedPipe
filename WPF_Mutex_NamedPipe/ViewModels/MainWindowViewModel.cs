19d5d9b128e68a3359fc1e0668095cc804d33bb2
lifework <kobayashi@lifework.tech>
2022-09-20 10:47:31 +0900

    チャットっぽいGUIを作成

--- WPF_Mutex_NamedPipe/ViewModels/MainWindowViewModel.cs
+++ WPF_Mutex_NamedPipe/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,60 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using Reactive.Bindings;
+using System;
+using System.Reactive.Disposables;
+using System.Security.Principal;
 
 namespace WPF_Mutex_NamedPipe.ViewModels
 {
-    public class MainWindowViewModel : BindableBase
+    public class MainWindowViewModel : BindableBase, IDisposable
     {
-        private string _title = "Prism Application";
-        public string Title
+        public IRegionManager RegionManager { get; private set; }
+        public DelegateCommand<object> SendCommand { get; private set; }
+        
+        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+        public ReactivePropertySlim<string?> Title { get; set; } = new("WPF_NamedPipe");
+        public ReactivePropertySlim<string?> ReceivedMessage { get; set; } = new();
+        public ReactivePropertySlim<string?> InputMessage { get; set; } = new();
+
+
+        public void Dispose()
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            Disposable.Dispose();
         }
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IRegionManager regionManager)
         {
+            RegionManager = regionManager;
+            SendCommand = new DelegateCommand<object>(OnSendCommand);
 
+            Disposable.Add(Title);
+            Disposable.Add(ReceivedMessage);
+            Disposable.Add(InputMessage);
+        }
+
+        private async void OnSendCommand(object parameter)
+        {
+            if (string.IsNullOrEmpty(InputMessage.Value))
+            {
+                return;
+            }
+            if (AppendReceivedMessage(InputMessage.Value))
+            {
+                InputMessage.Value = "";
+            }
+        }
+
+        private bool AppendReceivedMessage(string message)
+        {
+            ReceivedMessage.Value += Now() + " " + InputMessage.Value + "\n";
+            return true;
+        }
+
+        private string Now()
+        {
+            return "[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "]";
         }
     }
 }
